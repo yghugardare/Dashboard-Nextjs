deb201b4d4218fd8b76fb5d3d9f0ccf39b4a4081
Yash Ghugardare <yghugardare6@gmail.com>
2024-02-08 21:42:51 +0530

    styling in nextjs

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
 import AcmeLogo from '@/app/ui/acme-logo';
 import { ArrowRightIcon } from '@heroicons/react/24/outline';
 import Link from 'next/link';
+import styles from '@/app/ui/home.module.css';
 
 export default function Page() {
   return (
     <main className="flex min-h-screen flex-col p-6">
+      <div className="h-0 w-0 border-b-[30px] border-l-[20px] border-r-[20px] border-b-black border-l-transparent border-r-transparent" />
+      {/* <div className={styles.shape} /> */}
       <div className="flex h-20 shrink-0 items-end rounded-lg bg-blue-500 p-4 md:h-52">
         {/* <AcmeLogo /> */}
       </div>
